9f1964076bad4752aa1649c33ae48474740b5e52
Akhilesh Yadav <Akhilesh.Yadav@gothiadigital.com>
2025-09-11 12:03:55 +0530

    Added example of registering listeners for events raised during lifecycle of a spring application

--- webapi/src/main/java/com/demo/webapi/Main.java
+++ webapi/src/main/java/com/demo/webapi/Main.java
@@ -20,6 +20,30 @@ public class Main {
             out.println("*      Welcome to the Web API App       *");
             out.println("******************************************");
         });
+
+        // During the lifecycyle of a spring application, various events are published to signal different stages of
+        // the application's startup and shutdown process. We can listen to these events by adding listeners.
+        // Here, we add a simple listener that prints the event class name to the console. But more sophisticated
+        // logic can be implemented based on the event type.
+
+        // Some events are actually triggered before the ApplicationContext is created, so we cannot register a
+        // listener on those as a @Bean. We can register them with the SpringApplication.addListeners(…​) method or
+        // the SpringApplicationBuilder.listeners(…​) method.
+
+        //If we want those listeners to be registered automatically, regardless of the way the application is created,
+        // we can add a META-INF/spring.factories file to our project and reference your listener(s) by using the
+        // ApplicationListener key, as shown in the following example:
+        //
+        //org.springframework.context.ApplicationListener=com.example.project.MyListener
+
+        // This is also how spring boot autoconfigurations are registered. In principle, the autoconfigurations module
+        // subscribes to the ApplicationEnvironmentPreparedEvent event to register additional property sources
+        // before the application context is created.
+
+        app.addListeners((event) -> {
+            System.out.println("Application event received: " + event.getClass().getName());
+        });
+
         app.run(args);
 //        SpringApplication.run(Main.class, args);
     }
