88641253e77b40f354d11cd2f3707fa23747ce29
Akhilesh Yadav <Akhilesh.Yadav@gothiadigital.com>
2025-11-13 22:55:17 +0530

    .

--- webapi/src/main/java/com/demo/Main.java
+++ webapi/src/main/java/com/demo/Main.java
@@ -1,21 +1,17 @@
-package com.demo.webapi;
+package com.demo;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 import java.util.Arrays;
 import java.util.logging.Logger;
-import org.springframework.kafka.annotation.EnableKafka;
 
 //By default, Spring Boot only scans for components (like @RestController, @ConfigurationProperties) in the same
-// package and sub-packages as the main application class. Since com.demo.controllers is not a sub-package of
-// com.demo.webapi, Spring Boot won't find our controller. Same with GreetingConfiguration in com.demo.domain.
-// To fix this, we can use scanBasePackages to tell Spring Boot to scan the com.demo package and all its sub-packages.
+// package and sub-packages as the main application class.
 
-@SpringBootApplication(scanBasePackages = "com.demo")
-@ConfigurationPropertiesScan(basePackages = "com.demo")
+@SpringBootApplication()
+@ConfigurationPropertiesScan()
 public class Main {
 
     public static void main(String[] args) {
@@ -51,10 +47,12 @@ public class Main {
         });
 
         var ctxt = app.run(args);
-//        SpringApplication.run(Main.class, args);
 
         // Get the list of beans registered in application context
-        Arrays.stream(ctxt.getBeanDefinitionNames()).forEach(Logger::info);
+        // Arrays.stream(ctxt.getBeanDefinitionNames()).forEach(Logger::info);
+
+        // Get the classpath for the application
+        // Arrays.stream(System.getProperty("java.class.path").split(":")).forEach(Logger::info);
     }
 
 }
