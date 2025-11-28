06616571bc11f51cdeb2618ccaa1209fce117e7a
Akhilesh Yadav <Akhilesh.Yadav@gothiadigital.com>
2025-11-28 19:56:05 +0530

    Added propetheus metrics exposition. Can be accessed at /actuator/prometheus

---
+++ webapi/src/main/java/com/demo/beans/DemoBeans.java
@@ -0,0 +1,13 @@
+package com.demo.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DemoBeans {
+
+    @Bean(name = "demoMessage")
+    public String getMessage() {
+        return "Hello from DemoBeans!";
+    }
+}
--- webapi/src/main/java/com/demo/controllers/GreetingController.java
+++ webapi/src/main/java/com/demo/controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package com.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,21 @@ import com.demo.domain.GreetingConfiguration;
 @RequestMapping("/api/greeting")
 class GreetingController {
 
-    public GreetingController(GreetingConfiguration greetingConfig) {
+    public GreetingController(GreetingConfiguration greetingConfig, @Qualifier("demoMessage") String demoMessage) {
         this.greetingConfig = greetingConfig;
+        this.demoMessage = demoMessage;
     }
     @GetMapping()
     public ResponseEntity<String> greetFromSetting() {
         return ResponseEntity.ok("Hello " + greetingConfig.getName() + ", " + greetingConfig.getMessage());
     }
 
+    @GetMapping("/frombean")
+    public ResponseEntity<String> greetFromBean() {
+        return ResponseEntity.ok("Message from bean " + demoMessage);
+    }
+
     /* State */
     private final GreetingConfiguration greetingConfig;
+    private final String demoMessage;
 }
