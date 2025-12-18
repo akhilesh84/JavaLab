31a5a99cffcf23fdc05ccda81dd700d0f73cd5be
Akhilesh Yadav <Akhilesh.Yadav@gothiadigital.com>
2025-12-18 19:23:22 +0530

    Added loki for logging

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -14,6 +14,9 @@ kubectl apply -f webapi-deployment.yaml
 # Deploy Prometheus
 kubectl apply -f prometheus-deployment.yaml
 
+# Deploy Loki
+kubectl apply -f loki-deployment.yaml
+
 # Deploy Grafana
 kubectl apply -f grafana-deployment.yaml
 
@@ -25,6 +28,7 @@ kubectl wait --for=condition=available --timeout=300s deployment/kafka -n k8slab
 kubectl wait --for=condition=available --timeout=300s deployment/kafka-ui -n k8slab-ns
 kubectl wait --for=condition=available --timeout=300s deployment/webapi -n k8slab-ns
 kubectl wait --for=condition=available --timeout=300s deployment/prometheus -n k8slab-ns
+kubectl wait --for=condition=available --timeout=300s deployment/loki -n k8slab-ns
 kubectl wait --for=condition=available --timeout=300s deployment/grafana -n k8slab-ns
 kubectl wait --for=condition=available --timeout=300s deployment/jaeger -n k8slab-ns
 
@@ -34,6 +38,7 @@ echo "Access services:"
 echo "  Kafka UI: kubectl port-forward -n k8slab-ns service/kafka-ui-service 8080:8080"
 echo "  WebAPI: kubectl port-forward -n k8slab-ns service/webapi-service 8080:8080"
 echo "  Prometheus: kubectl port-forward -n k8slab-ns service/prometheus-service 9090:9090"
+echo "  Loki: kubectl port-forward -n k8slab-ns service/loki-service 3100:3100"
 echo "  OTEL Collector (OPTIONAL): kubectl port-forward -n k8slab-ns deployment/webapi 8889:8889"
 echo "  Grafana: kubectl port-forward -n k8slab-ns service/grafana-service 3000:3000"
 echo "  Jaeger UI: kubectl port-forward -n k8slab-ns service/jaeger-service 16686:16686"
--- webapi/src/main/java/com/demo/controllers/StudentController.java
+++ webapi/src/main/java/com/demo/controllers/StudentController.java
@@ -2,6 +2,7 @@ package com.demo.controllers;
 
 import com.demo.model.Student;
 import com.demo.services.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+@Slf4j
 @ComponentScan(basePackages = {"com.demo.services"})
 @RestController
 @RequestMapping("/api/student")
@@ -23,17 +25,22 @@ class StudentController {
     }
 
     @GetMapping("/all")
-    public List<Student> getAllStudents() {
+    public List<Student> getAllStudents()
+    {
+        log.info("Getting all students");
         return studentService.getAllStudents();
     }
 
     @GetMapping("/{id}")
-    public Student getStudentById(@PathVariable(value = "id") Long id) {
+    public Student getStudentById(@PathVariable(value = "id") Long id)
+    {
+        log.info("Getting student with id {}", id);
         return studentService.getStudentById(id);
     }
 
     @PostMapping()
     public ResponseEntity<Student> createStudent(@Validated @RequestBody Student student) {
+        log.info("Creating student {}", student);
         Student created = studentService.createStudent(student);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
@@ -44,6 +51,7 @@ class StudentController {
 
     @PutMapping()
     public Student updateStudent(@Validated @RequestBody Student student) {
+        log.info("Updating student {}", student);
         return studentService.updateStudent(student);
     }
 
