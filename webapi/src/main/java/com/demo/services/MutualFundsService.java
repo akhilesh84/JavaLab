7497a14610bc99736cb15cee5660e7f076eb6998
Akhilesh Yadav <Akhilesh.Yadav@gothiadigital.com>
2025-12-20 11:00:23 +0530

    Switching downwards to using RestTemplate

--- webapi/src/main/java/com/demo/services/MutualFundsService.java
+++ webapi/src/main/java/com/demo/services/MutualFundsService.java
@@ -1,15 +1,12 @@
 package com.demo.services;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.URI;
-import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -19,63 +16,31 @@ import com.demo.domain.entities.mutualfunds.MutualFundScheme;
 @Service
 @Scope("singleton")
 public class MutualFundsService {
-    private final HttpClient _httpClient;
-    private final ObjectMapper _mapper;
-    static final String c_baseUrl = "https://api.mfapi.in/mf";
+    private final RestTemplate restTemplate;
+    private static final String c_BaseUrl = "https://api.mfapi.in/mf";
 
-    public MutualFundsService(){
-        _mapper = new ObjectMapper();
-        _mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        _httpClient = HttpClient.newHttpClient();
+    public MutualFundsService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public CompletableFuture<List<MutualFundScheme>> getAllSchemes() {
+        return CompletableFuture.supplyAsync(() ->
+                restTemplate.exchange(
+                        c_BaseUrl,
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<@NotNull List<MutualFundScheme>>() {}
+                ).getBody()
+        );
+    }
 
-    HttpRequest request = HttpRequest
-            .newBuilder()
-            .uri(URI.create(c_baseUrl))
-            .build();
-
-    return _httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-            .thenApply(response -> {
-                if (response.statusCode() != 200) {
-                    throw new RuntimeException("Failed to fetch data: " + response.statusCode());
-                }
-                return response;
-            })
-            .thenApply(HttpResponse::body)
-            .thenApply(jsonBody -> {
-                try {
-                    return _mapper.readValue(jsonBody,
-                        _mapper.getTypeFactory().constructCollectionType(List.class, MutualFundScheme.class));
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            });
-}
-
-    public CompletableFuture<FundHistoricData> getFundHistoricData(int fundId){
-
-        HttpRequest request = HttpRequest
-                .newBuilder()
-                .uri(URI.create(c_baseUrl + "/" + fundId))
-                .build();
-
-        return _httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(response -> {
-                    if (response.statusCode() != 200) {
-                        throw new RuntimeException("Failed to fetch data: " + response.statusCode());
-                    }
-                    return response;
-                })
-                .thenApply(HttpResponse::body)
-                .thenApply(jsonBody -> {
-                    try {
-                        return _mapper.readValue(jsonBody, FundHistoricData.class);
-                    }
-                    catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+    public CompletableFuture<FundHistoricData> getFundHistoricData(int fundId) {
+        return CompletableFuture.supplyAsync(() ->
+                restTemplate.getForObject(
+                        c_BaseUrl + "/" + fundId,
+                        FundHistoricData.class
+                )
+        );
     }
 }
+
